28acb9f2c5fb1be44f8de2699a8c09725b7f624f
Ahmed <aamohamed1999@gmail.com>
2024-07-21 13:37:45 +0100

    frontend update

--- main/views.py
+++ main/views.py
@@ -7,11 +7,11 @@ from main.back_end.api_manager import SkyscannerApi
 
 # Create your views here.
 def home(request):
-    sky = SkyscannerApi('london', 'New york', '2', '2024-02-13', '2024-02-20')
-    context = {
-        'flights': sky.flights(),
-    }
-    return render(request, 'home.html', context)
+    # sky = SkyscannerApi('london', 'New york', '2', '2024-02-13', '2024-02-20')
+    # context = {
+    #     'flights': sky.flights(),
+    # }
+    return render(request, 'home.html')
 
 
 
