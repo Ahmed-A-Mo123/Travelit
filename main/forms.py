4c14984d2695c1583fa9d8014524209f16e43e86
Ahmed <aamohamed1999@gmail.com>
2024-07-24 20:17:34 +0100

    a few changes to html using bootstrap

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,19 @@
 from django import forms
+from .models import SearchFields
 
 
 class Search(forms.Form):
-    origin = forms.CharField(label="Origin", required=True)
-    destination = forms.CharField(label="Destination", required=True)
-    departure_date = forms.DateField(widget=forms.SelectDateWidget(empty_label="Nothing"), required=True)
-    return_date = forms.DateField(widget=forms.SelectDateWidget(empty_label="Nothing"), required=True)
+    origin = forms.CharField(label="Origin", required=True, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter Origin',
+    }))
+    destination = forms.CharField(label="Destination", required=True, widget= forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter Destination'
+    }))
+    departure_date = forms.DateField(widget=forms.SelectDateWidget(empty_label="Nothing", attrs={
+        'class': 'form-select'
+    }), required=True)
+    return_date = forms.DateField(widget=forms.SelectDateWidget(empty_label="Nothing", attrs={
+        'class': 'form-select'
+    }), required=True)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class SearchFields(models.Model):
+    origin = models.CharField(max_length=30)
+    destination = models.CharField(max_length=30)
+    departure_date = models.DateField(name="departure_date")
+    return_date = models.DateField(name="return_date")
